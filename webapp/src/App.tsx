3c074f2d9eed00f73326250f412b2767cd98dbc7
MrMatiasz13 <mateuszgrzeskow1328@gmail.com>
2025-03-21 07:15:27 +0100

    Initial commit

---
+++ webapp/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h2 className="text-4xl font-bold">Hello World</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
